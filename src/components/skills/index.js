684097a0d46535a9115a9428983d17bb6ed1a729
smartbadger <connorbailes@gmail.com>
2020-12-10 14:41:36 -0700

    skill change on click

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,61 +1,90 @@
 import { Heading, Flex, Grid, Card } from "atoms/ui/elements";
-import LazyImage from 'atoms/ui/lazyimage'
-import { FillerStyles, ContainerStyles, SkillItem, SkillTab, SkillText, SkillName} from "./styles";
+import LazyImage from "atoms/ui/lazyimage";
+import {
+  FillerStyles,
+  ContainerStyles,
+  SkillItem,
+  SkillTab,
+  SkillText,
+  SkillName,
+  SkillTabsContainer,
+} from "./styles";
 import { useState, useEffect } from "react";
-
-const getSkillLevel = (value) => {
-  switch(true) {
-    case value >= 0.7:
-      return 'expert'
-      break;
-    case value < 0.7 && value >= 0.5:
-      return 'intermediate'
-    default:
-      return 'beginner'
-  }
-}
-const SkillLevel = ({ value }) => {
-  return (
-    <>
-      <SkillText>{getSkillLevel(value)}</SkillText>
-      <ContainerStyles>
-        <FillerStyles value={value} />
-      </ContainerStyles>
-    </>
-  );
-};
-
-const CreateSkill = ({ skill, category, image, lvl }) => {
-  return (
-    <SkillItem data-category={category}>
-      <Card>
-        <Flex flexDirection="column" alignItems="center" justify="center">
-          <LazyImage {...image} />
-          <SkillName>{skill}</SkillName>
-          <SkillLevel value={lvl} />
-        </Flex>
-      </Card>
-    </SkillItem>
-  );
-};
-
-const buildSkillGroups = (skills) => {
-  const group = {};
-  skills.forEach((skillObj, index) => {
-    const { category } = skillObj;
-    if (!group[category] || !group[category].length) {
-      group[category] = [];
-    }
-    group[category].push(<CreateSkill {...skillObj} key={`skill-${index}`} />);
-  });
-  return group;
-};
-
-const CategoryTabs = ({categories, active}) => (<>{categories.map(c => <SkillTab key={`skilltab__${c}`} active={c== active} >{c}</SkillTab>)}</>)
 
 const Skills = ({ title, skills }) => {
   const [category, setCategory] = useState(skills[0].category);
+  const getSkillLevel = (value) => {
+    switch (true) {
+      case value >= 0.7:
+        return "expert";
+      case value < 0.7 && value >= 0.5:
+        return "intermediate";
+      default:
+        return "beginner";
+    }
+  };
+
+  const SkillLevel = ({ value }) => {
+    return (
+      <>
+        <SkillText>{getSkillLevel(value)}</SkillText>
+        <ContainerStyles>
+          <FillerStyles value={value} />
+        </ContainerStyles>
+      </>
+    );
+  };
+
+  const CreateSkill = ({ skill, category, image, lvl }) => {
+    return (
+      <SkillItem data-category={category}>
+        <Card>
+          <Flex flexDirection="column" alignItems="center" justify="center">
+            <LazyImage {...image} />
+            <SkillName>{skill}</SkillName>
+            <SkillLevel value={lvl} />
+          </Flex>
+        </Card>
+      </SkillItem>
+    );
+  };
+
+  const buildSkillGroups = (skills) => {
+    const group = {};
+    skills.forEach((skillObj, index) => {
+      const { category } = skillObj;
+      if (!group[category] || !group[category].length) {
+        group[category] = [];
+      }
+      group[category].push(
+        <CreateSkill {...skillObj} key={`skill-${index}`} />
+      );
+    });
+    return group;
+  };
+  const changeSkill = ({ target }) => {
+    if (target) {
+      setCategory(target.dataset.category)
+    }
+  };
+
+  const CategoryTabs = ({ categories, active }) => (
+    <SkillTabsContainer>
+      {categories.map((c) => (
+        <SkillTab
+          key={`skilltab__${c}`}
+          active={c == active}
+          data-category={c}
+          onClick={changeSkill}
+        >
+          {c}
+        </SkillTab>
+      ))}
+    </SkillTabsContainer>
+  );
   const SkillGroup = buildSkillGroups(skills);
+
+
   return (
     <>
       <Heading>{title}</Heading>
--- src/components/skills/styles.js
+++ src/components/skills/styles.js
@@ -1,10 +1,20 @@
 import styled from "styled-components";
-import { Text } from "atoms/ui/elements";
+import { Text, Box } from "atoms/ui/elements";
 
 export const SkillItem = styled.div``;
 
 export const SkillTab = styled(Text)`
   font-weight: ${(props) => (props.active ? "bold" : "normal")};
+  color: ${(props) => (props.active ? props.theme.colors.blue : props.theme.colors.black )};
+  border-bottom: 2px solid ${(props) => (props.active ? props.theme.colors.blue : props.theme.colors.disabled )};
+  padding: 0 1rem;
+  text-align: center;
+  cursor: pointer;
+  opacity: ${(props) => (props.active ? 1 : 0.5 )};
+  transition: .25s opacity ease-in-out;
+  &:hover, &:focus, &focus-within {
+    opacity: ${(props) => (props.active ? 1 : 0.75 )};;
+  }
 `;
 
 export const ContainerStyles = styled.div`
@@ -33,4 +43,11 @@ export const SkillText = styled(Text)`
 export const SkillName = styled(Text)`
     font-weight: bold;
     letter-spacing: 1px;
+`
+export const SkillTabsContainer = styled(Box)`
+    display: flex;
+    flex-wrap: no-wrap;
+    justify-content: center;
+    align-items: center;
+    margin: 3rem 0;
 `
